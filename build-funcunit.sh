e5fbaaa8e43a30a5262f3046f2fa92676d4a6025
John Tregoning <tregoning@gmail.com>
2013-07-31 23:51:33 -0700

    Updated script to build latest version of funcunit

--- build-funcunit.sh
+++ build-funcunit.sh
@@ -2,42 +2,28 @@
 ###############################################################################
 # File:		build-funcunit.sh
 #
-# Description:  Script to build FuncUnit from source, translated from the instructions 
-#		found here: https://github.com/jupiterjs/funcunit/raw/master/README into a script 
-#		Some modifications have been added as required in order to build the project.
-#		Changes/Additions from original instructions are marked under "Note:"
+# Description:  Script to build a standalone version of FuncUnit from source
 #
 # Author: 	John Tregoning
 #
 ###############################################################################
 
-#Clone the repository at http://github.com/jupiterjs/javascriptmvc
-git clone git://github.com/jupiterjs/javascriptmvc.git
 
-#Use git submodule init and git submodule update to get the submodules.  The only ones we'll need are steal and funcunit
-#Note: jQuery & Syn are also required
-cd javascriptmvc
-git submodule init
-git submodule update
+git clone https://github.com/bitovi/funcunit.git
+
 cd funcunit
-git checkout master
-cd ../steal
-git checkout master
-cd ../jquery
-git checkout master
-
-cd ../funcunit
 git submodule init
 git submodule update
-cd syn
-git checkout master
+npm install promised-io
+npm install node-phantom
 
+cd ..
 
-#Make whatever changes you want to syn, funcunit, drivers, or selenium. 
-#***Your changes go here***
+#bespoke stealconfig file
+curl -O https://gist.github.com/tregoning/6128825/raw/e17a8b2b6c81aaefcf0b6593c649241c1e70eedf/stealconfig.js
+curl -O http://code.jquery.com/jquery-1.10.2.min.js
 
-#From the framework directory, on the command line run js funcunit/build.js.  This will build the JS scripts and copy the relevant files.
-cd ../../
-./js funcunit/build.js
+curl -O https://raw.github.com/bitovi/javascriptmvc/master/js
+chmod +x ./js
 
-#That's it, funcunit/dist contains the same standalone funcunit placed into the standalone download.
+git clone https://github.com/bitovi/steal.git
